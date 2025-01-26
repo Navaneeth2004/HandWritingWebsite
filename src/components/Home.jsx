2a48086a44949abffc791898bad3d5d80b7ec58e
Navaneeth <Navaneeth2004pa@gmail.com>
2025-01-26 14:57:32 +0530

    f

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React,{useState,useRef} from 'react';
 
 const Home = () => {
 
-  const handleClick = () => {
-    alert("Button clicked!");
-  };
+  const photo = useRef(null);
+  const [image,setimage] = useState(null);
+
+  const handleclick = () => {
+
+    photo.current.click();
+
+  }
+
+  const handlephoto = (event) => {
+
+    const image = event.target.files[0];
+    setimage(image)
+
+  }
+
+  const processbutton = () => {
+
+
+  }
   
   return (
     <>
@@ -14,8 +31,18 @@ const Home = () => {
       <div class="flex justify-center items-center bg-blue-200" style={{height: '85vh'}}>
 
         <div class="flex flex-col items-center">
-          <input class="hover:bg-violet-700 p-2 bg-violet-500 font-bold" type='file' />
-          <img class="mt-10" src='' alt='No image'/>
+          <input class="hover:bg-violet-700 p-2 bg-violet-500 font-bold" onChange={handlephoto} type='file' ref={photo}/>
+          {image && (
+            <>
+              <img
+              class="mt-10"
+              src={URL.createObjectURL(image)}
+              width="300"
+              onClick={handleclick}
+              />
+              <button class="mt-10 bg-blue-500 p-3 hover:bg-blue-700" onClick={processbutton}>Process</button>
+            </>
+          )}
         </div>
         <div class="flex ml-10 bg-yellow-50 p-50 outline outline-4 outline-blue-800">
           <h2>Output</h2>
